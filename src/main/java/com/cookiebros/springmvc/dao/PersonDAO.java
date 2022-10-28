06e52244714b6f49ca586195d4a043f2c7963e38
muskratman <balerka25@gmail.com>
2022-10-28 17:54:01 +0300

    //autoincremect

--- src/main/java/com/cookiebros/springmvc/dao/PersonDAO.java
+++ src/main/java/com/cookiebros/springmvc/dao/PersonDAO.java
@@ -2,10 +2,14 @@ package com.cookiebros.springmvc.dao;
 
 import com.cookiebros.springmvc.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -34,8 +38,7 @@ public class PersonDAO {
     }
 
     public void save(Person savedPerson) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?, ?)",
-                ++PEOPLE_COUNT,
+        jdbcTemplate.update("INSERT INTO Person(name, surname, age, email) VALUES(?, ?, ?, ?)",
                 savedPerson.getName(),
                 savedPerson.getSurname(),
                 savedPerson.getAge(),
@@ -54,4 +57,32 @@ public class PersonDAO {
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
     }
+
+
+    ////////test batch upate
+//    public void testBatchUpdate() {
+//        List<Person> people = create1000people();
+//        jdbcTemplate.batchUpdate("INSERT INTO People VALUES(?, ?, ?, ?, ?)", new BatchPreparedStatementSetter() {
+//            @Override
+//            public void setValues(PreparedStatement ps, int i) throws SQLException {
+//                ps.setInt(1, people.get(i).getId());
+//                ps.setString(1, people.get(i).getName());
+//                ps.setString(1, people.get(i).getSurname());
+//                ps.setInt(1, people.get(i).getAge());
+//                ps.setString(1, people.get(i).getEmail());
+//            }
+//            @Override
+//            public int getBatchSize() {
+//                return people.size();
+//            }
+//        });
+//    }
+//
+//    public List<Person> create1000people() {
+//        List<Person> people = new ArrayList<>();
+//        for (int i=0; i<1000; i++) {
+//            people.add(new Person(i, "Name"+i, "Surname"+i, (byte) 30, "email"+i+"@gmail.com"));
+//        }
+//        return people;
+//    }
 }
