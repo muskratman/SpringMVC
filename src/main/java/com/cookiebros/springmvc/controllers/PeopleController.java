984bd8daca2003818af88ba7038e09c12c8f5221
muskratman <balerka25@gmail.com>
2022-11-03 16:51:11 +0300

    //validate

--- src/main/java/com/cookiebros/springmvc/controllers/PeopleController.java
+++ src/main/java/com/cookiebros/springmvc/controllers/PeopleController.java
@@ -1,21 +1,27 @@
 package com.cookiebros.springmvc.controllers;
 
 import com.cookiebros.springmvc.dao.PersonDAO;
-import com.cookiebros.spring.models.Person;
+import com.cookiebros.springmvc.models.Person;
+import com.cookiebros.springmvc.util.PersonValidator;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.annotation.Target;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
-    public PeopleController(PersonDAO personDAO) {
+    @Autowired
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -36,19 +42,33 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String createNewPerson(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
-    public String update(@PathVariable("id") int id, Model model) {
+    public String edit(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "/people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@PathVariable("id") int id, @ModelAttribute("person") Person person) {
+    public String update(@PathVariable("id") int id, @ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
